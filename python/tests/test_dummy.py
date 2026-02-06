5923a1d9b5c2cc9f5fa7a925c3ff70fcec7f7beb
Claudio Carvalho <cclaudio@ibm.com>
2026-02-05 22:49:06 -0500

    python/tests: Add dummy test
    pytest requires at least one test case to be defined otherwise it
    returns the error 5 meaning that zero test cases was found.
    
    Signed-off-by: Claudio Carvalho <cclaudio@ibm.com>

---
+++ python/tests/test_dummy.py
@@ -0,0 +1,6 @@
+def test_dummy():
+    """
+    pytest requires at least one test to be defined otherwise it returns the
+    error 5 meaning that zero test cases was found.
+    """
+    assert True
