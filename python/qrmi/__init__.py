09e3710572d649ac5eb664efb57a5c67a683a244
Matthew Treinish <mtreinish@kortar.org>
2026-04-30 13:30:45 -0400

    Simplify Rust object re-exports in Python package
    This commit simplifies the mechanism by which the Rust defined data
    structures are exposed to the root of the Python package. Previously the
    use of importlib and manually inserting values into globals is making a
    lot of underlying assumptions about the structure of the python
    environment to work correctly and was also making something very simple
    overly complicated. The simplest way to handle the re-export is to just
    import them into the qrmi python namespace, just doing that and they'll
    be public as part of the Python qrmi package. This makes that change to
    correct this.
    
    One thing I would typically recommend to pair with this is adding an
    __all__ dunder attribute to the qrmi package to make it explicit which
    objects are publically available in the package instead of relying on
    what's been imported or defined in a module. But I opted not to make
    this change in this PR because it requires deciding more explicitly what
    should be public and that is better handled in isolation.

--- python/qrmi/__init__.py
+++ python/qrmi/__init__.py
@@ -14,8 +14,4 @@
 
 """QRMI python package"""
 
-from importlib import import_module as _im
-
-_core = _im(__name__ + "._core")  # -> qrmi._core  (Rust)
-
-globals().update({k: v for k, v in _core.__dict__.items() if not k.startswith("_")})
+from ._core import *
