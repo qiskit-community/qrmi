f42f7be25a9d586905fed6f013721e39cdec8c3f
cornallj <james.cornall@stfc.ac.uk>
2026-07-06 14:07:55 +0100

    Add sphinx-design and grid card support

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,9 @@ release = "2025"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_design",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
