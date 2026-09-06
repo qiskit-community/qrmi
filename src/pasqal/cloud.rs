d3b661bd567d62fc769f9cafb94d2d70d86fc69c
Munetaka Ohtani <ohtanim@jp.ibm.com>
2026-09-07 01:01:53 +0900

    restore task_log in pasqal cloud

--- src/pasqal/cloud.rs
+++ src/pasqal/cloud.rs
@@ -190,6 +190,10 @@ impl PasqalCloud {
         })
     }
 
+    async fn task_logs(&mut self, _task_id: &str) -> Result<String> {
+        Ok("There are no logs for this job.".to_string())
+    }
+
     fn task_kind(&self, task_id: &str) -> PasqalTaskKind {
         match self.task_kinds.get(task_id).copied() {
             Some(kind) => kind,
