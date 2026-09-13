75755885f259211aaa46f8643384c62e29f57144
Munetaka Ohtani <ohtanim@jp.ibm.com>
2026-09-14 02:33:38 +0900

    fix build errors

--- examples/qrmi/c/alice_bob_felis/src/felis.c
+++ examples/qrmi/c/alice_bob_felis/src/felis.c
@@ -15,8 +15,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "qrmi.h"
 #include "common.h"
+#include "qrmi.h"
 
 int main(int argc, char *argv[]) {
 
@@ -52,16 +52,27 @@ int main(int argc, char *argv[]) {
     qrmi_string_free(resource_id);
   }
 
-  bool is_accessible = false;
-  rc = qrmi_resource_status_is_accessible(qrmi, &is_accessible);
+  QrmiResourceStatus *res_status = NULL;
+  rc = qrmi_resource_status(qrmi, &res_status);
   if (rc == QRMI_RETURN_CODE_SUCCESS) {
-    if (is_accessible == false) {
-      fprintf(stderr, "%s cannot be accessed.\n", argv[1]);
+    bool is_accessible = false;
+    rc = qrmi_resource_status_is_accessible(res_status, &is_accessible);
+    qrmi_resource_status_free(res_status);
+    if (rc == QRMI_RETURN_CODE_SUCCESS) {
+      if (is_accessible == false) {
+        fprintf(stderr, "%s cannot be accessed.\n", argv[1]);
+        goto error;
+      }
+    } else {
+      const char *last_error = qrmi_get_last_error();
+      fprintf(stderr, "qrmi_resource_status_is_accessible() failed. %s (%d)\n",
+              last_error, qrmi_get_last_error_kind());
+      qrmi_string_free((char *)last_error);
       goto error;
     }
   } else {
     const char *last_error = qrmi_get_last_error();
-    fprintf(stderr, "qrmi_resource_status_is_accessible() failed. %s (%d)\n",
+    fprintf(stderr, "qrmi_resource_is_accessible() failed. %s (%d)\n",
             last_error, qrmi_get_last_error_kind());
     qrmi_string_free((char *)last_error);
     goto error;
